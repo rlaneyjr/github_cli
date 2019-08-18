7f9a6113d59418aac26c3d523e9c5ed566f2b80f
Ricky Laney <rlaneyjr@gmail.com>
2019-08-18 15:03:51 -0400

    Created 'scripts' directory

--- scripts/github_user_clone.sh
+++ scripts/github_user_clone.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# Example: ./scripts/clone_user.sh rlaneyjr
+# Example: ./scripts/github_user_clone.sh rlaneyjr
 # Save our location before we start moving around
 _pwd=$PWD
 # Only variable we want is the username
@@ -8,11 +8,16 @@ _user=$1
 # Capitalize the username first letter (directory name repos are stored)
 _cap_user=${_user^}
 # Capitalize the username completely
-_full_cap_user=${_user^^}
+#_full_cap_user=${_user^^}
 # Grab a list of the user's public repos (100 max)
 repos=`gh_list $_user | grep html_url | sed 's:^.*\s::'`
 # My personal directory for github users I am following (CHANGE IT!)
-cd /Volumes/T5-SSD/repos/Following && mkdir -p $_cap_user && cd $_cap_user
+_gitfollowdir="/Volumes/T5-SSD/repos/Following"
+_gituserdir="/Volumes/T5-SSD/repos/Following/$_cap_user"
+if ! [[ -d $_gituserdir ]]
+    then mkdir -p $_gituserdir && cd $_gituserdir
+    else cd $_gituserdir
+fi
 # Iterate and clone each repo
 for r in $repos; do
     git clone --recursive $r;
